70f952c4b15132fe5c81020a850c674d39c3cbef
RendeR <93490632+RendeR66@users.noreply.github.com>
2022-12-28 13:15:54 +0300

    Add files via upload

---
+++ deadinside.cpp
@@ -0,0 +1,31 @@
+/* https://taplink.cc/render */
+#include <iostream>
+#ifdef _WIN32
+#include <windows.h> // Виндовс
+#else 
+#include <unistd.h> // Линукс
+#endif
+#include <clocale>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int i = 1000;
+    int ded = 0;
+    std::cout << "У меня нет проблем" << std::endl;
+    std::cout << "Кроме моей башки" << std::endl;
+    std::cout << "Прости, я умер" << std::endl;
+    while (i > 8)
+    {
+        ded = i - 7;
+        std::cout << i << " - 7 = " << ded << std::endl;
+        i -= 7;
+#ifdef _WIN32
+        Sleep(1);
+#else
+        sleep(1);
+#endif
+    }
+    std::cout << "Теперь вы дединсайдик" << std::endl;
+    return 0;
+}
